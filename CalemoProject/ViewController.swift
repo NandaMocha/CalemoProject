48b1366f11473e6076a02a02d1745dc61baa782e
NandaMochammad <nanda.mocha@gmail.com>
2019-07-01 16:59:52 +0700

    Initial Commit

---
+++ CalemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalemoProject
+//
+//  Created by Nanda Mochammad on 01/07/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
