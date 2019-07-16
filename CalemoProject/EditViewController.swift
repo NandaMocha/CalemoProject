259556c9ec99b83b8eb58068a6e894588163471a
Martin Nate <martin.nate.contact@gmail.com>
2019-07-16 15:47:32 +0700

    everything contstrained

--- CalemoProject/EditViewController.swift
+++ CalemoProject/EditViewController.swift
@@ -11,12 +11,10 @@ import UIKit
 class EditViewController: UIViewController {
     
     @IBOutlet weak var futureField: UITextField!
-    @IBOutlet weak var q4Field: UITextField!
     @IBOutlet weak var q3Field: UITextField!
     @IBOutlet weak var q2Field: UITextField!
     @IBOutlet weak var q1Field: UIView!
     @IBOutlet weak var futureLabel: UILabel!
-    @IBOutlet weak var q4Label: UILabel!
     @IBOutlet weak var q3Label: UILabel!
     @IBOutlet weak var q2Label: UILabel!
     @IBOutlet weak var q1Label: UILabel!
@@ -26,6 +24,28 @@ class EditViewController: UIViewController {
     @IBOutlet weak var causeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Edit journal"
+        
+        emotionLabel.text = "Pretty bad"
+        causeLabel.text = "Workload"
+        
+        q1Label.text = "So what exactly happened earlier?"
+        q1Label.lineBreakMode = .byWordWrapping
+        q1Label.numberOfLines = 0
+        
+        q2Label.text = "What are the things you'd want to change?"
+        q2Label.lineBreakMode = .byWordWrapping
+        q2Label.numberOfLines = 0
+        
+        q3Label.text = "Is it possible to change that? If not what's the workaround?"
+        q3Label.lineBreakMode = .byWordWrapping
+        q3Label.numberOfLines = 0
+        
+        futureLabel.text = "Notes for future me:"
+        futureLabel.lineBreakMode = .byWordWrapping
+        futureLabel.numberOfLines = 0
+        
+        
 
         // Do any additional setup after loading the view.
     }
--- CalemoProject/ViewController.swift
+++ CalemoProject/ViewController.swift
@@ -11,12 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var futureAnswer: UILabel!
-    @IBOutlet weak var q4Answer: UILabel!
     @IBOutlet weak var q3Answer: UILabel!
     @IBOutlet weak var q2Answer: UILabel!
     @IBOutlet weak var q1Answer: UILabel!
     @IBOutlet weak var futureLabel: UILabel!
-    @IBOutlet weak var q4Label: UILabel!
     @IBOutlet weak var q3Label: UILabel!
     @IBOutlet weak var q2Label: UILabel!
     @IBOutlet weak var q1Label: UILabel!
@@ -27,6 +25,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var dateLabel: UINavigationItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "July 16, 2019"
         emotionLabel.text = "Pretty bad"
         causeLabel.text = "Workload"
         q1Label.text = "So what exactly happened earlier?"
@@ -40,9 +39,7 @@ class ViewController: UIViewController {
         q3Label.text = "Is it possible to change that? If not whats the workaround?"
         q3Label.lineBreakMode = .byWordWrapping
         q3Label.numberOfLines = 0
-        q4Label.text = "So whats the next step?"
-        q4Label.lineBreakMode = .byWordWrapping
-        q4Label.numberOfLines = 0
+
         futureLabel.text = "Notes for future me:"
         futureLabel.lineBreakMode = .byWordWrapping
         futureLabel.numberOfLines = 0
@@ -55,14 +52,11 @@ class ViewController: UIViewController {
        // q2Answer.frame = CGRect(x: self.q2Answer.frame.origin.x, y: self.q2Answer.frame.origin.y, width: q2Answer.intrinsicContentSize.width, height: q2Answer.intrinsicContentSize.height)
         q2Answer.lineBreakMode = .byWordWrapping
         q2Answer.numberOfLines = 0
-        q3Answer.text = "I don’t think I can change the amount of workload… I think I’d have better motivation if I had friends towork with tho"
+        q3Answer.text = "I don’t think I can change the amount of workload… I think I’d have better motivation if I had friends to work with tho"
        // q3Answer.frame = CGRect(x: self.q3Answer.frame.origin.x, y: self.q3Answer.frame.origin.y, width: q3Answer.intrinsicContentSize.width, height: q3Answer.intrinsicContentSize.height)
         q3Answer.lineBreakMode = .byWordWrapping
         q3Answer.numberOfLines = 0
-        q4Answer.text = "I’ll try and give Willy and Mocha a call, I’m sure they’re as stressed as I am and at least we can work and have fun together I think"
-       // q4Answer.frame = CGRect(x: self.q4Answer.frame.origin.x, y: self.q4Answer.frame.origin.y, width: q4Answer.intrinsicContentSize.width, height: q4Answer.intrinsicContentSize.height)
-        q4Answer.lineBreakMode = .byWordWrapping
-        q4Answer.numberOfLines = 0
+
         futureAnswer.text = "Don’t forget to get some snacks before we get together!"
         futureAnswer.lineBreakMode = .byWordWrapping
         futureAnswer.numberOfLines = 0
