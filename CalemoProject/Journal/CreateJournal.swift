2260585ef0b9a0520071db41d6ff152133343cd0
Wilbert <xuwilbert@gmail.com>
2019-07-20 12:02:36 +0700

    lanjuttan kemarin

--- CalemoProject/Journal/CreateJournal.swift
+++ CalemoProject/Journal/CreateJournal.swift
@@ -17,14 +17,14 @@ class CreateJournal: UIViewController {
     var x : CGFloat?
     var y : CGFloat?
     
-    let progress = Progress(totalUnitCount: 10)
+//    let progress = Progress(totalUnitCount: 10)
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        progressView.transform = progressView.transform.scaledBy(x: 333, y: 12)
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 2)
         progressView.progressTintColor = #colorLiteral(red: 0.9244551063, green: 0.7166977525, blue: 0.4467653632, alpha: 1)
-            progressView.trackTintColor = #colorLiteral(red: 0.5921100378, green: 0.5921833515, blue: 0.5920851827, alpha: 1)
+
         
         x = Centering.center.x
         y = Centering.center.y
@@ -37,24 +37,29 @@ class CreateJournal: UIViewController {
     func centeringStart() {
         UIView.animate(withDuration: 4, delay: 0, options: [.autoreverse, .repeat], animations: {
             self.Centering.transform = CGAffineTransform.identity.scaledBy(x: 1.6, y: 1.6)
+//            self.progressView.setProgress(1, animated: true)
         }) { (done) in
-            self.Centering.frame = CGRect(x: 45, y: 45, width: 154, height: 154)
             
-            self.progressView.progress = 0.0
+//            self.progressView.progress = 0.0
             //            self.progress.completedUnitCount = 0
         }
         
         var  i : Int = 0
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
             true
-            if i == 5{ self.performSegue(withIdentifier: "centeringEnd", sender: self)
+            if i == 30{ self.performSegue(withIdentifier: "centeringEnd", sender: self)
             }
             i += 1
-            self.progressView.progress += 1 / 5
+            
+            UIView.animate(withDuration: 30.0) {
+                self.progressView.setProgress(1.0, animated: true)
+            }
+//            UIView.animate(withDuration: <#T##TimeInterval#>, animations: <#T##() -> Void#>)
+//            self.progressView.progress += 1 / 5
         })
         
         //                self.progress.completedUnitCount += 1
-        self.progressView.setProgress(Float(self.progress.fractionCompleted), animated: true)
+//        self.progressView.setProgress(Float(self.progress.fractionCompleted), animated: true)
         
     }
 }
---
+++ ProtocolClass/Protocol.swift
@@ -0,0 +1,9 @@
+//
+//  Protocol.swift
+//  CalemoProject
+//
+//  Created by Wilbert xu on 19/07/19.
+//  Copyright © 2019 nandamochammad. All rights reserved.
+//
+
+import Foundation
