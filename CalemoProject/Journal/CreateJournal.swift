c1893faaac25fd7c5b1e3fa02d9ac658ccdc550a
Khairani Ummah <khairani.u@gmail.com>
2019-07-21 22:26:32 +0700

    Add inhale exhale on centering & default emoji on first question

--- CalemoProject/Journal/CreateJournal.swift
+++ CalemoProject/Journal/CreateJournal.swift
@@ -10,45 +10,74 @@ import UIKit
 
 class CreateJournal: UIViewController {
     
+    @IBOutlet weak var inhaleText: UILabel!
     @IBOutlet weak var Centering: UIView!
     @IBOutlet weak var centeringBorder: UIView!
     @IBOutlet weak var progressView: UIProgressView!
     
-    var x : CGFloat?
-    var y : CGFloat?
+    var x1 : CGFloat?
+    var y1 : CGFloat?
+    var x2 : CGFloat?
+    var y2 : CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         progressView.transform = progressView.transform.scaledBy(x: 1, y: 2)
         progressView.progressTintColor = #colorLiteral(red: 0.9244551063, green: 0.7166977525, blue: 0.4467653632, alpha: 1)
-
+        createCentering()
         
-        x = Centering.center.x
-        y = Centering.center.y
-   
-        Centering.backgroundColor = UIColor(patternImage: (UIImage(named: "CC")!))
-        centeringBorder.backgroundColor = UIColor(patternImage: (UIImage(named: "CCBg")!))
+        
         
     }
     
+    func createCentering(){
+        x1 = Centering.center.x
+        y1 = Centering.center.y
+        x2 = centeringBorder.center.x
+        y2 = centeringBorder.center.y
+        Centering.layer.cornerRadius = Centering.frame.size.width/2
+        Centering.clipsToBounds = true
+        Centering.backgroundColor = #colorLiteral(red: 0.4235294118, green: 0.4078431373, blue: 0.4235294118, alpha: 1)
+        centeringBorder.layer.cornerRadius = centeringBorder.frame.size.width/2
+        centeringBorder.backgroundColor = #colorLiteral(red: 0.7339008451, green: 0.8349817395, blue: 0.7938916683, alpha: 1)
+    }
     func centeringStart() {
-        UIView.animate(withDuration: 4, delay: 0, options: [.autoreverse, .repeat], animations: {
-            self.Centering.transform = CGAffineTransform.identity.scaledBy(x: 1.6, y: 1.6)
-        }) { (done) in
+        UIView.animate(withDuration: 5, delay: 0, options: [.autoreverse, .repeat], animations: {
+            self.Centering.transform = CGAffineTransform.identity.scaledBy(x: 2, y: 2)
+        }) { (done)
+            in
         }
         
+       
+        
         var  i : Int = 0
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+        Timer.scheduledTimer(withTimeInterval: 4, repeats: true, block: { (timer) in
             true
-            if i == 5{ self.performSegue(withIdentifier: "centeringEnd", sender: self)
+            if i == 5{
+                self.performSegue(withIdentifier: "centeringEnd", sender: self)
             }
             i += 1
-            
-            UIView.animate(withDuration: 5.0) {
+            UIView.animate(withDuration: 20.0) {
                 self.progressView.setProgress(1.0, animated: true)
             }
+            
         })
+        var timerBreath : Int = 0
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+            true
+            
+            if (timerBreath == 0 || timerBreath == 2 || timerBreath == 4  ) {
+                self.inhaleText.text = "Exhale"
+            } else {
+                self.inhaleText.text = "Inhale"
+            }
+            timerBreath += 1
+            print(timerBreath)
+
+        })
+        
+        
         
     }
 }
