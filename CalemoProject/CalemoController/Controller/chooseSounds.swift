736f8c16928674db12117899e0750277f0dab5de
nandamocha <nanda.mocha@gmail.com>
2019-07-21 17:44:26 +0700

    Prepare to push

--- CalemoProject/CalemoController/Controller/chooseSounds.swift
+++ CalemoProject/CalemoController/Controller/chooseSounds.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2019 nandamochammad. All rights reserved.
 //
 
-import UIKit
+import AVKit
 
 class chooseSounds: UIViewController {
 
     let arrayOfSounds = ["Light Rain", "Heavy Rain", "Bird Singing", "Ocean Waves"]
+    var sounds = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,5 +33,65 @@ extension chooseSounds: UITableViewDelegate, UITableViewDataSource{
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //remove the checkmark
+        for i in 0 ..< arrayOfSounds.count{
+            let index = IndexPath(item: i, section: 0)
+            tableView.cellForRow(at: index)?.accessoryType = .none
+        }
+        
+        tableView.reloadData()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //add checkmark to selected row
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        switch indexPath.row {
+        case 0:
+            sounds = "lightrain"
+        case 1:
+            sounds = "heavyrain"
+        case 2:
+            sounds = "birdsinging"
+        default:
+            sounds = "oceanwaves"
+        }
+        
+        playSound(sounds: sounds)
+        
     
+    }
+    
+}
+
+extension chooseSounds: AVAudioPlayerDelegate{
+    func playSound(sounds: String){
+        print("Play sounds, ", sounds)
+        
+        let player: AVAudioPlayer?
+        
+        guard let url = Bundle.main.url(forResource: sounds, withExtension: "mp3") else {print("Data Not Found"); return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+
+            player.prepareToPlay()
+            player.delegate = self
+            player.numberOfLoops = -1
+            
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            
+            player.play()
+            
+        } catch let error as NSError {
+            print(error.description)
+        }
+        
+        DataManager.shared.favoriteSounds = sounds
+    }
 }
